61e00249beef32d060b8d549e0d674ceead6cb29
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-01-25 21:51:03 +0100

    Add files via upload

---
+++ Job Scrapers/oficinaempleo.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+URL = "https://www.oficinaempleo.com/buscar/ofertas/cloud-engineer"
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+]
+
+service = Service()
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-agent={random.choice(user_agents)}")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get(URL)
+time.sleep(3)
+
+scroll_increment = 0
+while scroll_increment < 500:
+    driver.execute_script(f"window.scrollTo(0, {scroll_increment * 10});")
+    time.sleep(random.uniform(1, 2))
+    scroll_increment += 50
+
+page_source = driver.page_source
+driver.quit()
+
+soup = BeautifulSoup(page_source, "html.parser")
+
+boxes = soup.find_all("div", class_="row col-dsk")
+for box in boxes:
+    job_title = box.find('a', class_='launch_ext_link')['title']
+    print(job_title)
