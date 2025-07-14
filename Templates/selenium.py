5727ccf9e57322ec6454f92bf546329d003fd3a0
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2025-07-14 07:48:53 +0200

    added some import and an action to click a button

--- Templates/selenium.py
+++ Templates/selenium.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 import time
 
 URL = "ENTER URL"
@@ -12,6 +16,13 @@ driver = webdriver.Chrome()
 driver.get(URL)
 
 time.sleep(3)
+
+# Click a button
+try:
+    some_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, 'ENTER XPATH HERE')))
+    some_button.click()
+except TimeoutException:
+    print("No button found.")
 
 page_source = driver.page_source
 
